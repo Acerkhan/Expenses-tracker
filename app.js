abc24f7d32d01a54cb4449f900a92af96d770922
sta-muhammad <hazratxmuhammad@gmail.com>
2026-08-07 15:20:46 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const SUPABASE_URL = 'https://inptsochtqsarxyjdqkv.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlucHRzb2NodHFzYXJ4eWpkcWt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1NzE1MzMsImV4cCI6MjEwMTE0NzUzM30.Nhvb2IrCBfqvznvD_j0lMwFbWqsRSdmrkaOfHpVXqR4';
-let transactions = [];
 
 let transactions = [];
 let cashflowChartInstance = null;
